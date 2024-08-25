5e7cbab111e3ebaef9bf5ea7768af10836217996
Pavlo Ukhan <pavlo@ukhan.in.ua>
2024-08-26 02:21:38 +0300

    Fixed getLinkTitle for quickAddTask

--- src/helpers/link.js
+++ src/helpers/link.js
@@ -1,14 +1,26 @@
 export default function getLinkTitle(href) {
-  return new Promise(resolve => {
-    let code = `
-      Array.from( document.querySelectorAll("[href='${href}']") )
-        .map(el => el.innerText)
-        .sort((t1, t2) => t1.length - t2.length)
-        .pop()
-    `;
-
-    chrome.tabs.executeScript({ code }, function(results) {
-      resolve(results.pop());
+  return new Promise((resolve) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs) {
+        chrome.scripting.executeScript(
+          {
+            func: (href) => {
+              const r = Array.from(
+                document.querySelectorAll(`[href='${href}']`)
+              )
+                .map((el) => el.innerText)
+                .sort((t1, t2) => t1.length - t2.length)
+                .pop();
+              return r;
+            },
+            args: [href],
+            target: { tabId: tabs[0].id },
+          },
+          (res) => {
+            resolve(res[0].result);
+          }
+        );
+      }
     });
   });
 }
